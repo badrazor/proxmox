156963c06f0add8077751c089c8eb3838859b886
Alexey Egorov <aegorov@mednetstudy.com>
2017-03-07 18:06:24 +0300

    changes in qemu-launch.sh

--- qemu-launch.sh
+++ qemu-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $1 =~ /version/]; then
+if [ $1 =~ /version/ ]; then
   echo "QEMU emulator version 2.7.0 pve-qemu-kvm_2.7.0-9, Copyright (c) 2003-2016 Fabrice Bellard and the QEMU Project developers"
 if
 
