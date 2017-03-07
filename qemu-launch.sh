889b0434177a9110998b13712dd1d815b4894da8
Alexey Egorov <aegorov@mednetstudy.com>
2017-03-07 18:06:05 +0300

    changes in qemu-launch.sh

--- qemu-launch.sh
+++ qemu-launch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ARGS=echo $@ | sed -e 's|-display||g' -e 's|-redir||g'
+if [ $1 =~ /version/]; then
+  echo "QEMU emulator version 2.7.0 pve-qemu-kvm_2.7.0-9, Copyright (c) 2003-2016 Fabrice Bellard and the QEMU Project developers"
+if
+
+ARGS=$(echo $@ | sed -e 's|-display||g' -e 's|-redir||g')
 
 exec /usr/bin/kvm $ARGS
