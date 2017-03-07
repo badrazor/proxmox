c7619ba795450f8566e0ecaa8d956690f41d3913
Alexey Egorov <aegorov@mednetstudy.com>
2017-03-07 16:10:37 +0300

    fixed file path in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ansible-playbook -e "args_template=args-centos.yml" -e "kernel=http://ftp.hosteurope.de/mirror/centos.org/7/os/x86_64/isolinux/vmlinuz" -e "initrd=http://ftp.hosteurope.de/mirror/centos.org/7/os/x86_64/isolinux/initrd.img" centos7.yml
+ansible-playbook -e "args_template=centos7-args.yml" \
+  -e "kernel=http://ftp.hosteurope.de/mirror/centos.org/7/os/x86_64/isolinux/vmlinuz" \
+  -e "initrd=http://ftp.hosteurope.de/mirror/centos.org/7/os/x86_64/isolinux/initrd.img" \
+  centos7.yml
